2a29f473264183c65a5103c9703fdd0dfc176565
Ajit Santossh <ajitsantossh09@gmail.com>
2023-09-08 17:24:45 +0100

    change of hero section content

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,8 +16,8 @@ const Hero = () => {
 
         <div className="flex flex-row justify-between items-center w-full">
           <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
-            The Next <br className="sm:block hidden" />{" "}
-            <span className="text-gradient">Gen</span>{" "}
+            Your next <br className="sm:block hidden" />{" "}
+            <span className="text-gradient">steps</span>{" "}
           </h1>
           <div className="ss:flex hidden md:mr-4 mr-0">
             <GetStarted />
@@ -25,7 +25,7 @@ const Hero = () => {
         </div>
 
         <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] w-full">
-          of Investments.
+          to Investments.
         </h1>
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
          Offering higher potential returns comaprared to traditional investments.
